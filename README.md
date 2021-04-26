e554c9bd981ea04264303bf3a3e7434031bd8d7e
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-04-26 14:15:46 +0900

    free_insertFirst_func_finish

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Doubly Circular Linked List
 
 ## To build
-- [] int freeList(listNode* h);
+- [O] int freeList(listNode* h);
+- [O] int insertFirst(listNode* h, int key);
 - [] int insertLast(listNode* h, int key);
-- [] int deleteLast(listNode* h);
-- [] int insertFirst(listNode* h, int key);
 - [] int deleteFirst(listNode* h);
-- [] int invertList(listNode* h);
+- [] int deleteLast(listNode* h);
 - [] int insertNode(listNode* h, int key);
 - [] int deleteNode(listNode* h, int key);
+- [] int invertList(listNode* h);
 
 
 
--- circular-linked-list.c
+++ circular-linked-list.c
@@ -43,7 +43,7 @@ int main()
 	listNode* headnode=NULL;
 
 	do{
-		printf("----------------------------------------------------------------\n");
+		printf("--------[2018038074]-----------------------[임종훈]-------------\n");
 		printf("                  Doubly Circular Linked List                   \n");
 		printf("----------------------------------------------------------------\n");
 		printf(" Initialize    = z           Print         = p \n");
@@ -114,49 +114,57 @@ int initialize(listNode** h) {
 
 	/* headNode에 대한 메모리를 할당하여 리턴 */
 	*h = (listNode*)malloc(sizeof(listNode));
-	(*h)->rlink = *h;
+	(*h)->rlink = *h;//헤드노드의 링크가 다 나를 가르키도록 한다.
 	(*h)->llink = *h;
-	(*h)->key = -9999;
+	(*h)->key = -9999;//헤드 노드에 값은 저장하지 않는다
 	return 1;
 }
 
 /* 메모리 해제 */
 int freeList(listNode* h){
+	listNode* p = h->rlink;//첫번째 노드의 주소
+	listNode* next_node = NULL;//다음 노드 임시저장
 
+	while(next_node != h){//헤드의 주소가 나올 때 까지
+		next_node = p->rlink;//다음 노드의 주소 임시저장
+		free(p);
+	}
+	//마지막에 헤드노드 해제
+	free(h);
 	return 0;
 }
 
 
 
-void printList(listNode* h) {
+void printList(listNode* h) {//연결리스트의 노드 출력 함수
 	int i = 0;
 	listNode* p;
 
 	printf("\n---PRINT\n");
 
-	if(h == NULL) {
+	if(h == NULL) {//헤드 노드도 없을 때 전처리
 		printf("Nothing to print....\n");
 		return;
 	}
 
 	p = h->rlink;
 
-	while(p != NULL && p != h) {
+	while(p != NULL && p != h) {//먼저 노드를 출력
 		printf("[ [%d]=%d ] ", i, p->key);
 		p = p->rlink;
 		i++;
 	}
-	printf("  items = %d\n", i);
+	printf("  items = %d\n", i);//노드의 갯수 출력
 
 
 	/* print addresses */
 	printf("\n---checking addresses of links\n");
 	printf("-------------------------------\n");
-	printf("head node: [llink]=%p, [head]=%p, [rlink]=%p\n", h->llink, h, h->rlink);
+	printf("head node: [llink]=%p, [head]=%p, [rlink]=%p\n", h->llink, h, h->rlink);//헤드 노드의 주소와 헤드 노드가 저장하고 있는 주소 출력
 
 	i = 0;
 	p = h->rlink;
-	while(p != NULL && p != h) {
+	while(p != NULL && p != h) {//노드의 주소와 링크마다 저장하고 있는 주소 출력
 		printf("[ [%d]=%d ] [llink]=%p, [node]=%p, [rlink]=%p\n", i, p->key, p->llink, p, p->rlink);
 		p = p->rlink;
 		i++;
@@ -189,6 +197,23 @@ int deleteLast(listNode* h) {
  * list 처음에 key에 대한 노드하나를 추가
  */
 int insertFirst(listNode* h, int key) {
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	listNode* r_node;//삽입할 위치의 오른쪽 노드
+
+	node->key = key;//값 저장
+	r_node = h->rlink;
+
+	if(r_node = h){//헤드노드 외에 아무런 노드도 없을 때
+		node->rlink = h;//노드의 끝의 표현은 헤드노드를 가르키는 것이다.
+		node->llink = h;
+		h->rlink = node;//첫 노드를 가르킴
+		h->llink = node;//마지막 노드를 가르킴
+	}
+
+	node->rlink = h->rlink;
+	node->llink = h;//첫 노드의 의미
+	h->rlink = node;
+	r_node->llink = node;//두번째가 된 노드가 가르키도록
 
 
 	return 1;
